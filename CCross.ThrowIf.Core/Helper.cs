8243ff7acddca240604089072f375226917bef5d
Philippe Matray <phmatray@outlook.com>
2016-01-19 16:59:43 +0100

    Initial

---
+++ CCross.ThrowIf.Core/Helper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace CCross.ThrowIf.Core
+{
+    public static class Helper
+    {
+        internal static string GetMemberName<T>(this Expression<Func<T>> expression)
+        {
+            if (expression == null)
+                throw new ArgumentNullException(nameof(expression));
+
+            var body = expression.Body as MemberExpression;
+            if (body == null)
+                throw new ArgumentException("Invalid argument", nameof(expression));
+
+            var argumentName = body.Member.Name;
+            return argumentName;
+        }
+
+        internal static T GetValue<T>(this Expression<Func<T>> expression)
+        {
+            if (expression == null)
+                throw new ArgumentNullException(nameof(expression));
+
+            var argumentValue = expression.Compile().Invoke();
+            return argumentValue;
+        }
+    }
+}
